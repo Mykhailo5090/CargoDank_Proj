bdefd8a0f216059a6ce6d61963bc5cb0cb88b6ba
Михайло Данканич <mihajlodankanic@MacBook-Air-Mihajlo.local>
2024-11-05 22:34:51 +0200

    partner end without bug

--- src/pages/Partner.jsx
+++ src/pages/Partner.jsx
@@ -6,11 +6,13 @@ import first from '../assets/images/partners/first.png';
 import second from '../assets/images/partners/second.png';
 import third from '../assets/images/partners/third.png';
 import fourth from '../assets/images/partners/fourth.png';
+import peoples from '../assets/images/partners/peoples.png';
+import teenager from '../assets/images/partners/teenagers.png';
 
 const Partner = () => {
   return (
     <div className="div__container__partner">
-      <div className="main__item main__item_1">
+      <div className="main__item main__item_1 div__partner__item">
         <img className="header-camion" src={headercamion} alt="header-camion" />
         <div className="camion-info __wrapper_1">
           <p className="p__camion__info">
@@ -68,8 +70,82 @@ const Partner = () => {
           </div>
         </div>
       </div>
-      <div className="div__partner__item_2"></div>
-      <div className="div__partner__item_3"></div>
+      <div className="div__partner__item_2 div__partner__item __wrapper__partner">
+        <p className="partners__text">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex, nulla
+          repellat unde excepturi incidunt iusto blanditiis fugiat adipisci,
+          aspernatur error similique officiis quas perspiciatis quos nam autem
+          ipsum, animi obcaecati? Facilis optio quia, deleniti fuga, voluptates
+          aspernatur reprehenderit quos accusamus fugiat sed ipsum accusantium
+          recusandae officia neque quam voluptas. Et tempora doloremque ipsa
+          nemo veniam neque quia, repudiandae pariatur accusamus? Maxime,
+          cupiditate molestiae. Beatae enim tempore aliquid quo nobis illo ad
+          totam, facere quibusdam ullam ea harum repudiandae sunt cupiditate, ut
+          nostrum assumenda! Blanditiis sit, vel deleniti consequatur hic vero.
+          At, unde. Consequatur, repellendus mollitia eligendi voluptate ullam
+          quasi placeat quae est saepe omnis, libero dolores ducimus repellat
+          quo hic corrupti iste, nisi voluptatem! Inventore odio sunt velit
+          laudantium incidunt? Aliquid porro ducimus sed harum ipsum
+          necessitatibus doloribus sequi iure, vero neque dolor, praesentium
+          culpa earum sapiente magni facere minus labore, sit placeat eos
+          voluptate? Consectetur voluptas dolores a numquam.
+        </p>
+        <div className="container__partners__1">
+          <img src={peoples} alt="peoples" className="img__partners__people" />
+          <p className="p__item__container__partners">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo,
+            cumque iste eius nisi perferendis esse perspiciatis alias nam et
+            iusto dolorem necessitatibus rem placeat obcaecati rerum? Fuga atque
+            eius dolor! Explicabo, odio provident amet, consectetur eum nobis
+            eius exercitationem at ipsum laboriosam minima error? Recusandae
+            officiis blanditiis nobis nostrum dolorum veritatis, suscipit
+            officia error maxime incidunt alias nam dolore saepe. Earum eos
+            molestias libero illo rerum, hic similique, recusandae distinctio
+            placeat quae alias numquam tenetur! Consequatur quas labore, ipsam
+            totam obcaecati accusamus quos aut itaque rem modi voluptates
+            dolorum ipsum! Neque, consectetur voluptatem. Commodi optio facilis
+            ea tempore nisi, iusto delectus assumenda, voluptatem quas esse
+            atque libero, numquam velit debitis amet ipsa quibusdam. Debitis
+            incidunt non esse assumenda pariatur eligendi. Facilis nesciunt
+            dolores eligendi facere accusantium vero corporis similique quia et
+            obcaecati. Atque enim fugit amet voluptatem totam perferendis ipsa
+            saepe similique sunt odio. Atque totam dolorem molestiae recusandae
+            voluptate. Sunt, qui sapiente. Numquam deserunt totam necessitatibus
+            quam dolorem placeat ipsum modi rem esse amet, quasi commodi eaque
+            corporis magni facere quos unde nulla aspernatur omnis iusto minus
+            corrupti quis? Error maiores, a optio quibusdam assumenda tempora.
+            Eligendi, quia quasi eaque incidunt ex rerum mollitia voluptatum
+            dolore accusamus, qui perferendis dolores recusandae obcaecati
+            tenetur atque reprehenderit dicta a, consequuntur in. Rerum omnis
+            libero consectetur! Iure exercitationem modi doloribus harum quis
+            excepturi voluptas sed, animi est. Tenetur culpa praesentium cum
+            voluptas dolores, quibusdam earum asperiores, ipsa tempora, sequi
+          </p>
+        </div>
+      </div>
+      <div className="div__partner__item_3 div__partner__item __wrapper__partner">
+        <p className="p__teenagers p__item__container__partners">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempore
+          consequuntur iusto itaque praesentium temporibus! Deleniti molestias
+          alias quas dolorem eveniet ipsa modi ut eos quos voluptate, atque
+          aperiam suscipit. Voluptatibus nemo quas tenetur illo officiis
+          voluptates quae eius! Culpa eos fuga officiis, commodi illo autem
+          veritatis quam sint perspiciatis, aperiam quis similique architecto
+          libero quia dignissimos, excepturi quas iste. Repellendus natus
+          reiciendis dolor cupiditate ducimus possimus aperiam? Perspiciatis,
+          corrupti? Quam eveniet sed perferendis, pariatur aut saepe recusandae
+          voluptatum veniam, eaque voluptas harum fugit maxime sint minus quia
+          nihil ipsa? Labore architecto fugiat rerum error id placeat a
+          incidunt, dolor corporis quam deleniti sequi rem commodi accusantium
+          voluptatibus praesentium fuga esse animi exercitationem libero
+          temporibus nihil magnam? Aut, minus nisi. Inventore eos, et commodi
+          fuga veritatis vitae atque dicta placeat officia consequuntur
+          assumenda. Provident molestias eligendi suscipit libero aliquid ab
+          iste, ad voluptatum. Praesentium adipisci quis autem harum aliquid
+          nobis.
+        </p>
+        <img src={teenager} alt="teenagers" className="img__teenagers" />
+      </div>
     </div>
   );
 };
